649fadc196f975cb7f0ff4f5593dcec2dcba4d84
Niaz <niaz.act@gmail.com>
2023-02-27 12:03:41 +0530

    Gui Day 3 todos Gui Updated

--- gui.py
+++ gui.py
@@ -1,11 +1,60 @@
 import PySimpleGUI as sg
-
+import functions
 label1 = sg.Text("Type in the To-Do")
 
-input_box1 = sg.InputText(tooltip="Enter Todo")
+input_box1 = sg.InputText(tooltip="Enter Todo",
+                          key="todo")
 add_button = sg.Button("Add")
+exit_button = sg.Button('Exit')
+listbox = sg.Listbox(values=functions.get_todos(),
+                     key="todos",
+                     enable_events=True,
+                     size=[45, 10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window('To Do List', layout=[[label1],[input_box1,add_button]])
-window.read()
+window = sg.Window('To Do List', layout=[[label1],
+                    [input_box1,add_button],
+                    [listbox,edit_button],
+                    [exit_button]],
+                   font=('Helvetica', 12))
+while True:
+    event, values=window.read()
+    print(f"event: {event}, values: {values}")
+    print(1,values['todo'])
+    print(2,values['todos'])
+
+
+    match event:
+        case 'Add':
+            local_todos=functions.get_todos();
+            new_todo = values['todo']
+            if new_todo !="":
+                local_todos.append(new_todo) + "\n"
+                functions.write_todos(local_todos)
+                window['todos'].update(values=local_todos)
+            else:
+                print("Invalid Data Cannot be Empty ")
+
+        case "todos":
+            edit_todo = values['todos'][0]
+            window['todo'].update(value=edit_todo)
+
+        case "Edit":
+
+            local_todos = functions.get_todos();
+            new_todo = values['todo']
+            edit_todo = values['todos'][0]
+            if new_todo != edit_todo :
+                index=local_todos.index(edit_todo)
+                local_todos[index]=new_todo
+                functions.write_todos(local_todos)
+                window['todos'].update(values=local_todos)
+
+        case sg.WIN_CLOSED:
+            break
+        case 'Exit':
+            break
+
+
 window.close()
 
